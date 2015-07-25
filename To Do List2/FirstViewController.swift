8cbd1c5a6955e6afc062ab78ae7d507f619072aa
jerrydream <r82zhang@uwaterloo.ca>
2015-07-25 16:08:58 -0400

    Initial Commit

---
+++ To Do List2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  To Do List2
+//
+//  Created by Ruijie Zhang on 2015-07-25.
+//  Copyright (c) 2015 Ruijie Zhang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ To Do List2/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  To Do List2
+//
+//  Created by Ruijie Zhang on 2015-07-25.
+//  Copyright (c) 2015 Ruijie Zhang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
